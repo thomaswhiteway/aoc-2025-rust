20dcca1107da6a74bfbb289023bc16cd0eda0460
Thomas Whiteway <thomas.whiteway@gmail.com>
2025-12-08 17:43:19 +0000

    Day 8 Part 2

--- src/day08.rs
+++ src/day08.rs
@@ -2,7 +2,7 @@ use failure::{Error, err_msg};
 use itertools::Itertools;
 use std::cmp::Reverse;
 
-fn make_connections(boxes: &[(i64, i64, i64)]) -> Vec<(usize, usize)> {
+fn make_connections(boxes: &[(i64, i64, i64)]) -> impl Iterator<Item = (usize, usize)> {
     boxes
         .iter()
         .enumerate()
@@ -15,60 +15,72 @@ fn make_connections(boxes: &[(i64, i64, i64)]) -> Vec<(usize, usize)> {
             (x_a - x_b).pow(2) + (y_a - y_b).pow(2) + (z_a - z_b).pow(2)
         })
         .map(|((box_a, _), (box_b, _))| (box_a, box_b))
-        .take(1000)
-        .collect()
 }
 
-fn find_circuits(connections: &[(usize, usize)]) -> Vec<Vec<usize>> {
-    let max_box = connections
-        .iter()
-        .flat_map(|(box_a, box_b)| [*box_a, *box_b])
-        .max()
-        .unwrap_or_default();
-    let mut circuits: Vec<Vec<usize>> = vec![];
-    let mut box_circuits: Vec<Option<usize>> = (0..=max_box).map(|_| None).collect();
+struct Circuits {
+    circuits: Vec<Vec<usize>>,
+    box_circuits: Vec<Option<usize>>,
+}
 
-    for &(box_a, box_b) in connections {
-        match (box_circuits[box_a], box_circuits[box_b]) {
+impl Circuits {
+    fn new(num_boxes: usize) -> Self {
+        Circuits {
+            circuits: vec![],
+            box_circuits: (0..num_boxes).map(|_| None).collect(),
+        }
+    }
+
+    fn connect(&mut self, box_a: usize, box_b: usize) {
+        match (self.box_circuits[box_a], self.box_circuits[box_b]) {
             (Some(circ_a), Some(circ_b)) => {
                 if circ_a != circ_b {
-                    let circuit_b = circuits.splice(circ_b..=circ_b, [vec![]]).next().unwrap();
-                    for &box_ix in circuit_b.iter() {
-                        box_circuits[box_ix] = Some(circ_a)
+                    let (merge_to, merge_from) = if circ_a > circ_b {
+                        (circ_b, circ_a)
+                    } else {
+                        (circ_a, circ_b)
+                    };
+
+                    let merging = self
+                        .circuits
+                        .splice(merge_from..=merge_from, [vec![]])
+                        .next()
+                        .unwrap();
+                    for &box_ix in merging.iter() {
+                        self.box_circuits[box_ix] = Some(merge_to)
                     }
-                    circuits[circ_a].extend(circuit_b);
+                    self.circuits[merge_to].extend(merging);
                 }
             }
             (Some(circ_a), None) => {
-                circuits[circ_a].push(box_b);
-                box_circuits[box_b] = Some(circ_a);
+                self.circuits[circ_a].push(box_b);
+                self.box_circuits[box_b] = Some(circ_a);
             }
             (None, Some(circ_b)) => {
-                circuits[circ_b].push(box_a);
-                box_circuits[box_a] = Some(circ_b);
+                self.circuits[circ_b].push(box_a);
+                self.box_circuits[box_a] = Some(circ_b);
             }
             (None, None) => {
-                let circ = circuits.len();
-                circuits.push(vec![box_a, box_b]);
-                box_circuits[box_a] = Some(circ);
-                box_circuits[box_b] = Some(circ);
+                let circ = self.circuits.len();
+                self.circuits.push(vec![box_a, box_b]);
+                self.box_circuits[box_a] = Some(circ);
+                self.box_circuits[box_b] = Some(circ);
             }
         }
     }
 
-    circuits.retain(|circ| !circ.is_empty());
-    circuits
-}
+    fn circuit_sizes(&self) -> Vec<usize> {
+        self.circuits
+            .iter()
+            .map(|circuit| circuit.len())
+            .filter(|&size| size > 0)
+            .sorted_by_key(|&size| Reverse(size))
+            .collect()
+    }
 
-fn solve_part_1(boxes: &[(i64, i64, i64)]) -> usize {
-    let connections = make_connections(boxes);
-    let mut circuits = find_circuits(&connections);
-    circuits.sort_by_key(|circuit| Reverse(circuit.len()));
-    (0..3)
-        .map(|idx| circuits.get(idx).map(|circ| circ.len()).unwrap_or(1))
-        .product()
+    fn is_fully_merged(&self) -> bool {
+        self.circuits.first().map(|circuit| circuit.len()).unwrap_or_default() == self.box_circuits.len()
+    }
 }
-
 pub struct Solver {}
 
 impl super::Solver for Solver {
@@ -96,7 +108,26 @@ impl super::Solver for Solver {
     }
 
     fn solve(boxes: Self::Problem) -> (Option<String>, Option<String>) {
-        let part1 = solve_part_1(&boxes);
-        (Some(part1.to_string()), None)
+        let mut circuits = Circuits::new(boxes.len());
+        let mut connections = make_connections(&boxes);
+
+        for _ in 0..1000 {
+            let (box_a, box_b) = connections.next().unwrap();
+            circuits.connect(box_a, box_b);
+        }
+
+        let circuit_sizes = circuits.circuit_sizes();
+        let part1: usize = (0..3).map(|idx| circuit_sizes[idx]).product();
+
+        let mut part2 = None;
+        while let Some((box_a, box_b)) = connections.next() {
+            circuits.connect(box_a, box_b);
+            if circuits.is_fully_merged() {
+                part2 = Some((boxes[box_a].0 * boxes[box_b].0).to_string());
+                break;
+            }
+        }
+
+        (Some(part1.to_string()), part2)
     }
 }
